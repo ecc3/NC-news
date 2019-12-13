5844a70038b9ca52084f0ea09a86a19a27078f53
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-12-13 16:30:00 +0000

    updated components in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
+import ArticlesList from "./components/ArticlesList";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <ArticlesList />
     </div>
   );
 }
