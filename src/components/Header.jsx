a543e34bf49a8087b9afd0366d1856b4326f89d4
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-12-13 14:54:07 +0000

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Welcome to Student Tracker</h1>
+      <p>User: username</p>
+    </div>
+  );
+};
+
+export default Header;
