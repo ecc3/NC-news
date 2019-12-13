16aeb4be3dcd20ad50acf29ed4f3500b35176892
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-12-13 16:29:02 +0000

    Added header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <div>
       <h1>Welcome to Student Tracker</h1>
       <p>User: username</p>
+      <Navbar />
     </div>
   );
 };
