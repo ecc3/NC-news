837b8834353db4e5450198db62e42db514ca38fd
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-12-16 16:41:13 +0000

    added random three article cards to welcome page

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
+import * as api from "../utils/api";
+import ArticleCard from "./ArticleCard";
 
-const Welcome = () => {
-  return (
-    <div>
-      <h1>NC-news</h1>
-      <h3>Get all your news!</h3>
-    </div>
-  );
-};
+class Welcome extends Component {
+  state = {
+    articles: []
+  };
+
+  componentDidMount = async () => {
+    if (this.state.articles.length === 0) {
+      const articles = await api.getAllArticles(undefined, "votes");
+      this.setState({ articles });
+    }
+  };
+
+  render() {
+    const { articles } = this.state;
+    return (
+      <div>
+        <h1>NC-news</h1>
+        <h3>Get all your news!</h3>
+        <ArticleCard
+          {...articles[Math.floor(Math.random() * articles.length)]}
+        />
+        <ArticleCard
+          {...articles[Math.floor(Math.random() * articles.length)]}
+        />
+        <ArticleCard
+          {...articles[Math.floor(Math.random() * articles.length)]}
+        />
+      </div>
+    );
+  }
+}
 
 export default Welcome;
+
+// import React from "react";
+
+// const Welcome = () => {
+//   return <div></div>;
+// };
+
+// export default Welcome;
