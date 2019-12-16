22c5105a4ae2915cff6965bfcab9055e54ad7541
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-12-16 12:53:34 +0000

    added date and comment_count to article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-const ArticleCard = ({ author, title, topic, votes, article_id }) => {
+const ArticleCard = ({
+  author,
+  title,
+  topic,
+  votes,
+  article_id,
+  comment_count,
+  created_at
+}) => {
   return (
     <div>
       <Link to={`/articles/${article_id}`}>
         <h3>{title}</h3>
       </Link>
       <h4>{author}</h4>
+      <h4>{created_at}</h4>
       <p>{topic}</p>
-      <p>{votes}</p>
+      <p>
+        Votes: {votes}, Comments: {comment_count}
+      </p>
     </div>
   );
 };
