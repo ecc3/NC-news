a422189438eb5d21d09f4138711f1658a6cfc171
Elizabeth <liz3.33zie@yahoo.co.uk>
2019-12-16 17:29:02 +0000

    display signed-in user details in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
 
-const Header = () => {
+const Header = props => {
   return (
     <div>
       <h1>Welcome to Student Tracker</h1>
-      <p>User: username</p>
+      <p>User: {props.user.name}</p>
+      <img src={props.user.avatar_url} alt="user's avatar" />
       <Navbar />
     </div>
   );
