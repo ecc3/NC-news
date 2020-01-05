20fe65d416fe351e3ea52100eb8a7ab3c56c3753
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-01-05 14:12:52 +0000

    created input box component

---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  color: #282c34;
+  font-size: 1em;
+  border-radius: 3px;
+  display: inline-block;
+  padding: 10px;
+  margin: 0px;
+  border: "2px solid #da7b93";
+  font-family: "Roboto", sans-serif;
+  //width: ${props => (props.primary ? "12rem" : "7rem")};
+`;
+
+export default Input;
