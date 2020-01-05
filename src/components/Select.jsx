e9261c8215a3ad982f76f6ec5998f134116259e5
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-01-05 14:14:20 +0000

    created Select component

---
+++ src/components/Select.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Select = styled.select`
+  color: #282c34;
+  font-size: 1em;
+  border-radius: 0px;
+  display: inline-block;
+  padding: 0.5rem 0;
+  margin: 0rem;
+  border: "2px solid #da7b93";
+  font-family: "Roboto", sans-serif;
+`;
+
+export default Select;
