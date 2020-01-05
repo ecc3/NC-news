63ecbcb52c74b6f559f4de0ffb669f39833414f8
Elizabeth <liz3.33zie@yahoo.co.uk>
2020-01-05 14:14:54 +0000

    created Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: ${props => (props.primary ? "#4056a1" : "transparent")};
+  color: ${props => (props.primary ? "white" : "#f13c20")};
+  font-size: 1em;
+  font-weight: bold;
+  border-radius: 0px;
+  display: inline-block;
+  padding: 0.5rem 0.5rem;
+  margin: 0rem;
+  min-width: 100px;
+  max-width: 200px;
+  border: none;
+  font-family: "Roboto", sans-serif;
+`;
+
+export default Button;
